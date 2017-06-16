b737ee0e1e9eaf76947b863270261211402315ad
jdecraecker@CRES <joris.decraecker@hotmail.com>
2017-06-16 17:01:12 +0200

    Added UWP and Basic socket

--- RbpWallpaperToLed.Service/Handlers/TransmissionHandlers.cs
+++ RbpWallpaperToLed.Service/Handlers/TransmissionHandlers.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
 
 namespace RbpWallpaperToLed.Service.Handlers
 {
@@ -10,6 +12,11 @@ namespace RbpWallpaperToLed.Service.Handlers
     {
         public static bool Send()
         {
+            Socket sListener;
+            SocketPermission permission = new SocketPermission(NetworkAccess.Accept, TransportType.Tcp, "", SocketPermission.AllPorts);
+            IPHostEntry ipHost = Dns.GetHostEntry("");
+            IPAddress ip = ipHost.AddressList[0];
+            
             return true;
         }
     }
